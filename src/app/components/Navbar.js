fb894f7eceb56f40156ecda2b17006ad38e7fdeb
jaskarandeogan <jaskaran2k15@gmail.com>
2024-05-02 15:01:03 -0700

    fix navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -41,15 +41,19 @@ const Navbar = () => {
                     <Image src="/sneakers.svg" alt="logo" width={100} height={30} className='h-auto w-auto cursor-pointer' onClick={
                         () => { window.location.href = '/' }
                     } />
-                    <li className="ml-8 px-2 cursor-pointer font-medium text-gray hover:brightness-125 hidden md:block">
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li className="px-2 cursor-pointer font-medium text-gray hover:brightness-125 hidden md:block">
-                        <Link href="/about">About</Link>
-                    </li>
+
                 </ul>
                 {loading ? null : !user ? (
                     <ul className="flex gap-4 items-center flex-row-reverse md:flex-row">
+                        <li className="ml-8 px-2 cursor-pointer font-medium text-gray hover:brightness-125 hidden md:block">
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li className="px-2 cursor-pointer font-medium text-gray hover:brightness-125 hidden md:block">
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li className="px-2 cursor-pointer font-medium text-gray hover:brightness-125 hidden md:block">
+                            <Link href="/profile">Profile</Link>
+                        </li>
                         <li className="cart cursor-pointer">
                             <Dropdowns Icon={HiOutlineShoppingCart}
                                 className={"shadow-sm border-2 border-zinc-300 hover:bg-gray-50 px-4 py-2"}
@@ -62,14 +66,23 @@ const Navbar = () => {
                         </button>
                     </ul>
                 ) : (
-                    <div className='flex flex-row-reverse md:flex-row items-center gap-2'>
-                        <div className="cart cursor-pointer mr-2">
+                    <ul className='flex flex-row-reverse md:flex-row items-center gap-2'>
+                        <li className="ml-8 px-2 cursor-pointer font-medium text-gray hover:brightness-125 hidden md:block">
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li className="px-2 cursor-pointer font-medium text-gray hover:brightness-125 hidden md:block">
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li className="px-2 cursor-pointer font-medium text-gray hover:brightness-125 hidden md:block">
+                            <Link href="/profile">Profile</Link>
+                        </li>
+                        <li className="cart cursor-pointer mr-2">
                             <Dropdowns Icon={HiOutlineShoppingCart}
                                 className={"shadow-sm border-2 border-zinc-300 hover:bg-gray-50 px-4 py-2"}
                             >
                                 <CartView />
                             </Dropdowns>
-                        </div>
+                        </li>
                         {user.photoURL &&
                             <Dropdowns image={{
                                 src: user?.photoURL,
@@ -84,12 +97,12 @@ const Navbar = () => {
                                 <ProfileView user={user} handleSignOut={handleSignOut} />
                             </Dropdowns>
                         }
-                        <div className='hidden md:flex items-center gap-4'>
+                        <li className='hidden md:flex items-center gap-4'>
                             <Link href="/profile">
                                 <p className='font-medium text-gray hover:brightness-125'>{user.displayName?.split(' ')[0] || user?.email}</p>
                             </Link>
-                        </div>
-                    </div>
+                        </li>
+                    </ul>
                 )}
             </div>
         </div>
